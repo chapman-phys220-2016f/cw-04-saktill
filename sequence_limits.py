c8afdf6d40ddf9bed222f25378d893b5fa845e0e
Sakthi Kasthurirengan <kasth100@mail.chapman.edu>
2016-09-23 20:01:16 +0000

    Added the first problem. Still has some bugs

--- sequence_limits.py
+++ sequence_limits.py
@@ -21,16 +21,16 @@ def limit(seq):
     given sequence if it passes a convergence test. """
     #limitTest1 = abs(seq[n]) - abs(seq[n+1])
     #limitTest2 = abs(seq[n-1]) - abs(seq[n])
-    for n in range(len(seq)):
+    for n in seq:
         if(abs(seq[n]) - abs(seq[n+1]) < abs(seq[n-1]) - abs(seq[n])):
            return seq[len(seq)-1]
         else:
-            print 'You have no limits. Reach for the stars.'
             return None
 
 def test_limit():
-    bn = lambda x: x
-
+    """We will test the limit function with the sequence b_n = n for n in the interval[0,500]"""
+    bn = np.linspace(0,500,501) 
+    limit(bn)
 
 def sequence_D(N):
     """Creates a specified sequence for evaluation. sin(2^(-n))/2^(-n)"""
@@ -38,7 +38,7 @@ def sequence_D(N):
     Dn = np.sin(2**(-n))/(2**(-n))
     return Dn
 
-def D(f,x,N):
+def E(f,x,N):
     """ """
     n = np.linspace(0,N,N+1)
     h = 2**(-n)
@@ -48,14 +48,13 @@ def D(f,x,N):
 def main():
     """The main function is being used to call the functions defined above and to test their limits."""
     an = sequence_a_n(100)
-    n = np.linspace(0,500,501)#this array should be what you get for b_n
     dn = sequence_D(1000) #sequence defined by sin(2^-n)/2^-n
-    d = D(lambda x: np.sin(x), 0,80)
-    
+    e = E(lambda x: np.sin(x), 0,80)
+    ePi = E(lambda x: np.sin(x),np.pi,80)
     print limit(an)
-    print limit(n)
     print limit(dn)
-    print limit(d)
+    print limit(e)
+    print limit(ePi)
    
 
 
